215e7f7c2f113de6aedd541827abdaa9d2a3affd
violet moon <violet.wmoon@gmail.com>
2019-01-23 19:43:58 -0600

    api query route

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 const express = require('express')
 const app = express()
+require('dotenv')
+
 const path = require('path')
 const router = express.Router()
+const fetch = require('node-fetch')
 
 const hostname = '0.0.0.0'
 const port = 2020
 
-const API_KEY = 'j1tzyTL48H4rZwLqnt88zknzzVLCCmJR'
+const urlHead = 'http://api.giphy.com/v1/gifs/search?q='
+const urlTail = `&api_key=${process.env.API_KEY}&limit=9`
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'/index.html'))
 })
 
-router.get('/:cat-jective', (req, res) => {
-	
-	res.json()	
+router.get('/:catjective', async (req, res) => {
+	let cats = await fetch(urlHead + req.param.catjective + '+cat' + urlTail)
+	let jsonCats = await cats.json()
+	res.json(jsonCats)
 }) 
 
 app.use('/', router)
 app.listen(process.env.port || port)
-console.log(`Magic happens on http://${hostname}:${port}`)
+console.log(`Witchcraft on aisle ${hostname}:${port}`)
